4af2cebba6541c52978f503955d0cceb640069a9
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-05 16:08:27 +0400

    commit 1

---
+++ lesson3.py
@@ -0,0 +1,35 @@
+import random
+class Human():
+    def __init__(self, name = 'Human', job = None, home=None,car=None):
+        self.name = name
+        self.money = 100
+        self.gladness = 50
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
+        def get_home(self):
+            pass
+        def get_car(self):
+            pass
+        def get_home(self):
+            pass
+class Auto:
+    def __init__(self, brand):
+        self.brand = brand
+        self.passengers = []
+    def add_passenger(self, human):
+        self.passengers.append(human)
+    def print_passengers_names(self):
+        if self.passengers !=[]:
+            print(f"Names of {self.brand} passengers:")
+            for passenger in self.passengers:
+                print(passenger.name)
+        else:
+            print(f"There are no passengers in {self.brand}")
+nick = Human("Nick")
+kate = Human("Kate")
+car = Auto("Mercedes")
+car.add_passenger(nick)
+car.add_passenger(kate)
+car.print_passengers_names()
