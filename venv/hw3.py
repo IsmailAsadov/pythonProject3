354aa7344297c69e59fe521cdf391b5d322a8adb
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-10 18:12:37 +0400

    commit 1

--- venv/hw3.py
+++ venv/hw3.py
@@ -1,12 +1,11 @@
-class toys:
-    ToysF = []
-    ToysM = []
-class dogs(nickname = None):
+class parts:
+    B = []
+    M = []
+brand= "None"
+class cars(brand):
     def __init__(self):
-        self.nickname = nickname
-foofy = dogs("Foofy")
-max = dogs("Max")
-toys_to_inputF = input("Choose a toy for Foofy")
-toys.ToysF.append(toys_to_inputF)
-toys_to_inputM = input("Choose a toy for Max")
-toys.ToysM.append(toys_to_inputM)
+        self.brand = brand
+bmw = cars("BMW")
+mercedes = cars("Mercedes")
+parts.B.append(input("What parts should we install in BMW:"))
+parts.M.append(input("What parts should we install in mercedes:"))
