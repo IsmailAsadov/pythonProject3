aeadb5a82d6e7faf9f8309a6e6cd4f9f0a851b4b
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-10 17:43:34 +0400

    commit 1

---
+++ venv/hw5.py
@@ -0,0 +1,12 @@
+from colorama import*
+#сбрасывает настройки после каждой строчки кода
+init(autoreset=True)
+#Цветной текст
+print(Fore.CYAN + " Hello")
+#Текст с цветным фоном
+print(Back.RED + "Hello")
+#Стиль текста
+print(Style.DIM + 'Hellllloooo')
+#сбрасывает настройки
+print(Style.RESET_ALL)
+print("HEY")
