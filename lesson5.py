e07368660ef5b700340d6d8bb8b85bd1d4b3cd32
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-10 16:44:44 +0400

    commit 1

---
+++ lesson5.py
@@ -0,0 +1,26 @@
+import requests
+import string
+import sys
+# def first_function():
+#     pass
+# class Human:
+#     pass
+# rq = requests
+# first_f = first_function
+# nick = Human
+# print(requests.__name__)
+# print(rq.__name__)
+# print(first_function.__name__)
+# print(first_f.__name__)
+# print(nick.__name__)
+# print(Human.__name__)
+# data = "string"
+# print(getattr(data,"startswith"))
+# print(getattr(data,"startswith", None))
+# print(getattr(data, "reverse", None))
+# data = "string"
+# def first_func():
+#     pass
+# print(callable(data))
+# print(callable(first_func))
+print(sys.platform)
