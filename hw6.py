ec5a6a5d744ee23baaaab151bfe702d641e42d88
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-14 23:16:50 +0400

    commit 1

---
+++ hw6.py
@@ -0,0 +1,13 @@
+from turtle import*
+shape('turtle') #TurtleGraphicsError
+pensize(100000) #syntaxError
+forward(100) #syntaxError
+left(90)
+forward(100)
+penup
+color('red', 'yellow') #syntaxError
+pendown #TypeError
+left(90)
+forward(100)#SyntaxError
+left(180)
+forward(150)
