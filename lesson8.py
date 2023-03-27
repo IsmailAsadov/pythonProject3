40ca5c5faa9d8c0d15aab499d352b0c2f717ed2d
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-27 16:59:49 +0400

    commit 1

--- lesson8.py
+++ lesson8.py
@@ -1,1 +1,10 @@
-from logging import*
+import logging
+logging.basicConfig(level = logging.DEBUG,
+                    filename="Logs.log",
+                    filemode="w",
+                    format="We have next logging message:%(asctime)s:%(levelname)s -%(message)s")
+logging.debug("debug")
+logging.info('info')
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
