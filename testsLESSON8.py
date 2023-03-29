77a8dba2dc817f927003a45614237ab39fe165b9
IsmailAsadov <ismailasad1010@gmail.com>
2023-03-29 17:41:58 +0400

    commit 1

--- testsLESSON8.py
+++ testsLESSON8.py
@@ -3,7 +3,6 @@ from lesson8 import*
 
 class My_test(unittest.TestCase):
     def test_args(self):
-        self.assertEqual(adder(2,2) 4)
-
-if __name__ = "__lesson8__":
-    unittest.main()
+        self.assertEqual(adder(2, 2), 4)
+if __name__ == "__lesson8__":
+    unittest.lesson8()
