7e0dc1dae270d1735dacd82dc9164d8f14a2e222
IsmailAsadov <ismailasad1010@gmail.com>
2023-04-16 13:21:09 +0400

    commit 1

--- lesson10.py
+++ lesson10.py
@@ -2,6 +2,20 @@ import sqlite3
 
 connection  = sqlite3.connect('itstep.sl3', 5)
 cur = connection.cursor
-cur.execute("CREATE TABLE first_table (name date_temp);")
+cur.execute("CREATE TABLE first_table (name);")
+cur.commit
+cur.execute("insert into first_table values(John)")
+cur.commit
+cur.execute("insert into first_table values(Kate)")
+cur.commit
+cur.execute("select in first_table rowid = 2")
+cur.commit
+cur.execute("update set name = Katherine where rowid = 2, ")
+cur.commit
+cur.execute("select in first_table rowid = 1")
+cur.commit
+cur.execute("delete from first_table where rowid = 1")
+cur.commit
+cur.execute('drop table first_table')
 cur.commit
 connection.close
